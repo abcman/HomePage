930c4e709ec0930c36d6a900d859eeed1bf0110e
lelcat <lolcat.is.spy@gmail.com>
2015-03-26 10:33:54 +0300

    11
    11

---
+++ org_action.php
@@ -0,0 +1,12 @@
+﻿ <?php
+ include ('db.php');
+ $name = $_POST['name']; 
+ $phone = $_POST['phone']; 
+ $adress = $_POST['adress']; 
+ $sql = 'INSERT INTO org(name, phone, adress) 
+ VALUES("'.$name.'", "'.$phone.'", "'.$adress.'")';
+ if(!mysql_query($sql))
+ {echo '<center><p><b>Ошибка при добавлении данных!</b></p></center>';} 
+ else 
+ {echo '<center><p><b>Данные добавлены!</b></p></center>';}
+?>
---
+++ org_form.php
@@ -0,0 +1,6 @@
+﻿<form method="post" action="org_action.php"> 
+  Введите наименование <input name="name" type="text">
+  Введите телефон: <input name="phone" type="text">
+  Введите адрес: <input name="adress" type="text">
+  <input value="Добавить организацию" type="submit">
+  </form>

48e9c8716e0d60d77ae20c3ab402a62c03d08744
Artemiy <qwerty.artem@gmail.com>
2015-03-27 11:34:51 +0400

    UBER

---
+++ glob.php
@@ -0,0 +1,49 @@
+﻿<p>
+ <center>
+ <form method="post" action="glob.php"> 
+  <br>Введите имя: <input size="30" name="name" type="text">
+ <br> Введите пароль: <input size="30" name="passw" type="text">
+  <br><input value="Добавить запись" type="submit">
+  </form></center></p>
+ 
+ <?php
+ include ('db.php');
+ 
+  $spisok = mysql_query("select * from users;");
+    if($spisok)
+    {
+      // Определяем таблицу и заголовок
+      echo "<table width=1000 border=0>";
+      echo "<tr><td>ID</td><td>Имя</td><td>Пароль</td><td>Дата</td></tr>";
+      // Так как запрос возвращает несколько строк, применяем цикл
+      while($user = mysql_fetch_array($spisok))
+      {
+        echo "<tr><td>".$user['id']."&nbsp;</td><td>".$user['name']."
+        &nbsp </td><td>".$user['passw']."&nbsp;</td><td>".  
+        $user['date']."&nbsp;</td></tr>";
+      }
+      echo "</table>";
+    }
+    else
+    {
+      echo "<p><b>Error: ".mysql_error()."</b><p>";
+      exit();
+    }
+ 
+ 
+ $name = $_POST['name']; // передаем переменной email значение глобального массива POST
+ $passw = $_POST['passw']; // повторять не буду: тут происходит то, что в первом случае
+ if ($passw != 0) {
+ $sql = 'INSERT INTO users(name, passw) 
+ VALUES("'.$name.'", "'.$passw.'")';
+ echo $name;
+// проверка
+ if(!mysql_query($sql))
+ {echo '<center><p><b>Ошибка при добавлении данных!</b></p></center>';} 
+ else 
+ {echo '<center><p><b>Данные добавлены!</b></p></center>';}
+ }
+ else { echo 'Данные не могут быть пустыми';}
+
+ ?>
+

756fdabaf839db0747fbdbb78adcb5cc9b55b1b2
Artemiy <qwerty.artem@gmail.com>
2015-04-02 13:58:54 +0300

    Stable 0.03
    Все подчистил. Исправил.
    - сейчас работаю над выборкой категорий

--- org_form.php
+++
@@ -1,6 +0,0 @@
-﻿<form method="post" action="org_action.php"> 
-  Введите наименование <input name="name" type="text">
-  Введите телефон: <input name="phone" type="text">
-  Введите адрес: <input name="adress" type="text">
-  <input value="Добавить организацию" type="submit">
-  </form>
